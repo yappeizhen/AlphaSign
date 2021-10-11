0922a4bc69e501e8857ffa78a51be057c7681709
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 <66234273+yappeizhen@users.noreply.github.com>
2021-10-11 20:00:29 +0800

    [Alphabet] Generate and display alphabet

--- ReactCV/src/components/DSButton.js
+++ ReactCV/src/components/DSButton.js
@@ -3,15 +3,16 @@ import React from "react";
 import styled from "styled-components"
 
 const StyledButton = styled.button`
+    visibility: ${props => props.visible ? "visible" : "hidden"};
     outline: none;
     border: none;
     cursor: pointer;
     display: flex;
     justify-content: center;
-    padding: 12px;
+    padding: 4px 12px;
     border-radius: 10px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.4);
-    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    transition: all 0.2s cubic-bezier(0.25, 0.8, 0.25, 1);
     width: auto;
     background-image: linear-gradient(
         to right,
@@ -19,13 +20,11 @@ const StyledButton = styled.button`
         rgb(248, 190, 393) 51%,
         rgb(258, 215, 280) 100%
     );
-    transition: 0.5s;
     background-size: 200% auto;
 
   &:hover {
     background-position: right center;
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-    margin: 8px 0px 10px;
   }
 `;
 const StyledText = styled.p`
@@ -35,10 +34,10 @@ const StyledText = styled.p`
         font-size: 12px;
     }
 `;
-function DSButton({ text }) {
+function DSButton({ text, onClick, visible = true }) {
 
     return (
-        <StyledButton>
+        <StyledButton onClick={onClick} visible={visible}>
             <StyledText>{text}</StyledText>
         </StyledButton>
     );
---
+++ ReactCV/src/constants/wordBank.js
@@ -0,0 +1,2 @@
+export const wordBank =
+  ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
