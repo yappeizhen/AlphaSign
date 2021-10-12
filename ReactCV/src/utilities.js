d47a7dc50f6fd06e6425abf0fb46930f2d6d4423
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 <66234273+yappeizhen@users.noreply.github.com>
2021-10-12 16:42:57 +0800

    [Validation] Added input validation and auto load next question

--- ReactCV/src/utilities.js
+++ ReactCV/src/utilities.js
@@ -11,56 +11,61 @@ const labelMap = {
 ///*
 const labelMap = {
 
-    1:{name:'A', color:'red'},
-    2:{name:'B', color:'yellow'},
-    3:{name:'C', color:'lime'},
-    4:{name:'D', color:'blue'},
-    5:{name:'E', color:'purple'},
-    6:{name:'F', color:'red'},
-    7:{name:'G', color:'yellow'},
-    8:{name:'H', color:'lime'},
-    9:{name:'I', color:'blue'},
-    10:{name:'J', color:'purple'},
-    11:{name:'K', color:'red'},
-    12:{name:'L', color:'yellow'},
-    13:{name:'M', color:'lime'},
-    14:{name:'N', color:'blue'},
-    15:{name:'O', color:'purple'},
-    16:{name:'P', color:'red'},
-    17:{name:'Q', color:'yellow'},
-    18:{name:'R', color:'lime'},
-    19:{name:'S', color:'blue'},
-    20:{name:'T', color:'purple'},
-    21:{name:'U', color:'red'},
-    22:{name:'V', color:'yellow'},
-    23:{name:'W', color:'lime'},
-    24:{name:'X', color:'blue'},
-    25:{name:'Y', color:'purple'},
-    26:{name:'Z', color:'red'},
+    1: { name: 'A', color: 'red' },
+    2: { name: 'B', color: 'yellow' },
+    3: { name: 'C', color: 'lime' },
+    4: { name: 'D', color: 'blue' },
+    5: { name: 'E', color: 'purple' },
+    6: { name: 'F', color: 'red' },
+    7: { name: 'G', color: 'yellow' },
+    8: { name: 'H', color: 'lime' },
+    9: { name: 'I', color: 'blue' },
+    10: { name: 'J', color: 'purple' },
+    11: { name: 'K', color: 'red' },
+    12: { name: 'L', color: 'yellow' },
+    13: { name: 'M', color: 'lime' },
+    14: { name: 'N', color: 'blue' },
+    15: { name: 'O', color: 'purple' },
+    16: { name: 'P', color: 'red' },
+    17: { name: 'Q', color: 'yellow' },
+    18: { name: 'R', color: 'lime' },
+    19: { name: 'S', color: 'blue' },
+    20: { name: 'T', color: 'purple' },
+    21: { name: 'U', color: 'red' },
+    22: { name: 'V', color: 'yellow' },
+    23: { name: 'W', color: 'lime' },
+    24: { name: 'X', color: 'blue' },
+    25: { name: 'Y', color: 'purple' },
+    26: { name: 'Z', color: 'red' },
 
 }
 //*/
 // Define a drawing function
-export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
-    for(let i=0; i<=boxes.length; i++){
-        if(boxes[i] && classes[i] && scores[i]>threshold){
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx, target) => {
+    for (let i = 0; i <= boxes.length; i++) {
+        if (boxes[i] && classes[i] && scores[i] > threshold) {
             // Extract variables
-            const [y,x,height,width] = boxes[i]
+            const [y, x, height, width] = boxes[i]
             const text = classes[i]
-            
+
             console.log(classes[i])
             console.log(scores[i])
             // Set styling
             ctx.strokeStyle = labelMap[text]['color']
             ctx.lineWidth = 10
             ctx.fillStyle = 'white'
-            ctx.font = '30px Arial'         
-            
+            ctx.font = '30px Poppins'
+
             // DRAW!!
             ctx.beginPath()
-            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
-            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/1.5);
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i] * 100) / 100, x * imgWidth, y * imgHeight - 10)
+            ctx.rect(x * imgWidth, y * imgHeight, width * imgWidth / 2, height * imgHeight / 1.5);
             ctx.stroke()
+
+            if (target && target.toUpperCase() === labelMap[text]['name']) {
+                return true;
+            };
         }
     }
+    return false;
 }
