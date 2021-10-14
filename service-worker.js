15444960d33e03ed23c83dc459cb28851d2217f5
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 <66234273+yappeizhen@users.noreply.github.com>
2021-10-14 19:04:34 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sign-Language-Image-Recognition/precache-manifest.f14c38aa4493a9450c28a934aea5e306.js"
+  "/Sign-Language-Image-Recognition/precache-manifest.dc470cf677d82ad96680a650f22ccc29.js"
 );
 
 self.addEventListener('message', (event) => {
