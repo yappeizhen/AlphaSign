714043e65261ea696feb1b9556052ccfff7a3bd7
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 <66234273+yappeizhen@users.noreply.github.com>
2021-10-14 19:54:32 +0800

    Updates

---
+++ precache-manifest.e64462607baa9ff27c3747481db0004d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82655a54d5b6b3028687771fa21af0c9",
+    "url": "/Sign-Language-Image-Recognition/index.html"
+  },
+  {
+    "revision": "4516852efc913b7fc13e",
+    "url": "/Sign-Language-Image-Recognition/static/css/main.bf467992.chunk.css"
+  },
+  {
+    "revision": "88810c1ee44c5aebeed4",
+    "url": "/Sign-Language-Image-Recognition/static/js/2.5b0e4423.chunk.js"
+  },
+  {
+    "revision": "d3e599f11755e164b0154acdfb5d2191",
+    "url": "/Sign-Language-Image-Recognition/static/js/2.5b0e4423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4516852efc913b7fc13e",
+    "url": "/Sign-Language-Image-Recognition/static/js/main.a47ee8a1.chunk.js"
+  },
+  {
+    "revision": "a7e6a957d105e4a2f62c",
+    "url": "/Sign-Language-Image-Recognition/static/js/runtime-main.603b2b57.js"
+  },
+  {
+    "revision": "859b4ac4de48bf325fa7d74d49cb6ced",
+    "url": "/Sign-Language-Image-Recognition/static/media/ASL_Alphabet.859b4ac4.png"
+  },
+  {
+    "revision": "7226b6cc1d9ab06570a9b32419d20b48",
+    "url": "/Sign-Language-Image-Recognition/static/media/background.7226b6cc.png"
+  },
+  {
+    "revision": "2d598a971aa717532854423e5b9adce7",
+    "url": "/Sign-Language-Image-Recognition/static/media/boy-hand.2d598a97.png"
+  },
+  {
+    "revision": "f221546ec773641d3e03b0509b7b4d39",
+    "url": "/Sign-Language-Image-Recognition/static/media/notebook.f221546e.png"
+  },
+  {
+    "revision": "08a9c3e6647f3bc7e9d1d2247745259f",
+    "url": "/Sign-Language-Image-Recognition/static/media/peace.08a9c3e6.png"
+  }
+]);
