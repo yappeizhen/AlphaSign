32174e1c83d0d329499610e817834f4df5fd3b40
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 <66234273+yappeizhen@users.noreply.github.com>
2021-10-20 01:18:51 +0800

    [Routing] Initialised route system

---
+++ ReactCV/src/pages/Baseline.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+import React from 'react';
+
+function Baseline() {
+
+    return (
+        <div className="App">
+            Hello
+        </div>
+
+    );
+}
+
+export default Baseline;
