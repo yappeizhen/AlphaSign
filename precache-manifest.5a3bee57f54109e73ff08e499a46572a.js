75e84b6a8b6cdb0043b0f527a2e2a5389b4110fa
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 <66234273+yappeizhen@users.noreply.github.com>
2021-10-24 14:32:04 +0800

    Updates

---
+++ precache-manifest.5a3bee57f54109e73ff08e499a46572a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c91cf2d237cfbafb70d6d796834183",
+    "url": "/Sign-Language-Image-Recognition/index.html"
+  },
+  {
+    "revision": "6016c9dfa8428a4d29cd",
+    "url": "/Sign-Language-Image-Recognition/static/css/main.642c0362.chunk.css"
+  },
+  {
+    "revision": "70fb34909680609a5383",
+    "url": "/Sign-Language-Image-Recognition/static/js/2.6aa818f0.chunk.js"
+  },
+  {
+    "revision": "ec80fe67bc5ee0f23ddda42b1c087092",
+    "url": "/Sign-Language-Image-Recognition/static/js/2.6aa818f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6016c9dfa8428a4d29cd",
+    "url": "/Sign-Language-Image-Recognition/static/js/main.33e99b9e.chunk.js"
+  },
+  {
+    "revision": "a7e6a957d105e4a2f62c",
+    "url": "/Sign-Language-Image-Recognition/static/js/runtime-main.603b2b57.js"
+  },
+  {
+    "revision": "28f3e993ab6864a4cea4b80843842cb7",
+    "url": "/Sign-Language-Image-Recognition/static/media/ASL_Alphabet.28f3e993.png"
+  },
+  {
+    "revision": "3ca4d72e11070faad4a7539480c60c20",
+    "url": "/Sign-Language-Image-Recognition/static/media/ASL_Alphabet_ABCD.3ca4d72e.png"
+  },
+  {
+    "revision": "7226b6cc1d9ab06570a9b32419d20b48",
+    "url": "/Sign-Language-Image-Recognition/static/media/background.7226b6cc.png"
+  },
+  {
+    "revision": "2d598a971aa717532854423e5b9adce7",
+    "url": "/Sign-Language-Image-Recognition/static/media/boy-hand.2d598a97.png"
+  },
+  {
+    "revision": "ff3521ecba25d8bdc867d519fb639ac2",
+    "url": "/Sign-Language-Image-Recognition/static/media/clouds_background.ff3521ec.jpeg"
+  },
+  {
+    "revision": "f221546ec773641d3e03b0509b7b4d39",
+    "url": "/Sign-Language-Image-Recognition/static/media/notebook.f221546e.png"
+  },
+  {
+    "revision": "08a9c3e6647f3bc7e9d1d2247745259f",
+    "url": "/Sign-Language-Image-Recognition/static/media/peace.08a9c3e6.png"
+  }
+]);
